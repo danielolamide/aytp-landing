38fc640e1d2ab25e70fa9a3f2d987025f44e9e50
CreamyMilk <creamym254@gmail.com>
2021-08-18 17:18:52 +0300

    updated teams slider

--- src/components/People.js
+++ src/components/People.js
@@ -4,57 +4,61 @@
 // import Teddy from "../assets/img/TeddyWaria.png";
 import React, { useEffect, useState } from "react";
 import { API_BASE_URL } from "../config";
-const FOUNDERS_URL = `${API_BASE_URL}/teams`
-
+const FOUNDERS_URL = `${API_BASE_URL}/teams`;
 
 function Person(props) {
-	let imageUrl = API_BASE_URL + props.source
-	let stakeHolderName = props.name
-	let currentRole = props.role
-	return (
-		<>
-			<div class="w-full md:w-1/4 p-3 flex flex-strech">
-				<div className="flex flex-col flex-strech w-full h-full">
-				<img alt="Rosemary" src={imageUrl} className="w-full h-4/5" />
-				<p className="text-black text-2xl font-bold text-center my-4" >{stakeHolderName}</p>
-				<p className="text-black text-center" >{currentRole}</p>
-				</div>
-
-			</div>
-		</>
-	);
-
+  let imageUrl = API_BASE_URL + props.source;
+  let stakeHolderName = props.name;
+  let currentRole = props.role;
+  return (
+    <>
+      <div class="w-full md:w-1/4 p-3 flex flex-strech">
+        <div className="flex flex-col flex-strech w-full h-full">
+          <img alt="Rosemary" src={imageUrl} className="w-full h-4/5" />
+          <p className="text-black text-2xl font-bold text-center my-4">
+            {stakeHolderName}
+          </p>
+          <p className="text-black text-center">{currentRole}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-
 export default function People() {
-	const [founders, setFounder] = useState([])
-	useEffect(() => {
-		fetch(FOUNDERS_URL)
-			.then(response => response.json())
-			.then((d) => {
-				setFounder(d)
-			})
-	}, [])
+  const [founders, setFounder] = useState([]);
+  useEffect(() => {
+    fetch(FOUNDERS_URL)
+      .then((response) => response.json())
+      .then((d) => {
+        setFounder(d);
+      });
+  }, []);
 
-	return (
-		<section className="py-8 bg-white border-b">
-			<div className="container flex flex-wrap pt-4 pb-22 mx-auto">
-				<h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-					Our Team
-				</h1>
-				<div className="w-full mb-4">
-					<div className="w-64 h-1 py-0 mx-auto my-0 rounded-t opacity-25 gradient"></div>
-				</div>
+  return (
+    <section className="py-8 bg-white border-b">
+      <div className="container flex flex-wrap pt-4 pb-22 mx-auto">
+        <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+          Our Team
+        </h1>
+        <div className="w-full mb-4">
+          <div className="w-64 h-1 py-0 mx-auto my-0 rounded-t opacity-25 gradient"></div>
+        </div>
 
-				{founders.map((value, index) => {
-					return <Person source={value["Photo"]["url"]} name={value["Name"]} role={value["Role"]}/>
-				})}
-				{/* <Person source={Rosemary}/>
+        {founders.map((value, index) => {
+          return (
+            <Person
+              source={value["Photo"]["url"]}
+              name={value["Name"]}
+              role={value["Role"]}
+            />
+          );
+        })}
+        {/* <Person source={Rosemary}/>
         <Person source={Diana}/>
         <Person source={Martin}/>
         <Person source={Teddy}/> */}
-			</div>
-		</section>
-	);
+      </div>
+    </section>
+  );
 }
